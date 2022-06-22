e162195825f90cc1649a07e568170e640e523fd4
Vinicius <viniciusdeoliveira16@hotmail.com>
2022-06-21 21:29:54 -0300

    Vinicius submeteu testes unitarios

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,70 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, getByLabelText, getByTestId } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verificação da existência da Calculadora Simples', () => {
+  const { getByText} = render(<App />);
+  const ElementoCalculadoraSimples = screen.getByText(/Calculadora Simples/i);
+  expect(ElementoCalculadoraSimples).toBeInTheDocument();
 });
+
+test('Verificação do botão AC', () => {
+  const { getByText } = render(<App />);
+  const elementoBotaoAC = screen.getByText(/AC/i);
+  expect(elementoBotaoAC).toBeInTheDocument();
+});
+
+test('Verificação do elemento 9', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero9 = screen.getByText(/9/i);
+  expect(elementoNumero9).toBeInTheDocument();
+});
+
+test('Verificação do elemento 8', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero8 = screen.getByText(/8/i);
+  expect(elementoNumero8).toBeInTheDocument();
+});
+
+test('Verificação do elemento 7', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero7 = screen.getByText(/7/i);
+  expect(elementoNumero7).toBeInTheDocument();
+});
+
+test('Verificação do elemento 6', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero6 = screen.getByText(/6/i);
+  expect(elementoNumero6).toBeInTheDocument();
+});
+
+test('Verificação do elemento 5', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero5 = screen.getByText(/5/i);
+  expect(elementoNumero5).toBeInTheDocument();
+});
+
+test('Verificação do elemento 4', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero4 = screen.getByText(/4/i);
+  expect(elementoNumero4).toBeInTheDocument();
+});
+
+test('Verificação do elemento 3', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero3 = screen.getByText(/3/i);
+  expect(elementoNumero3).toBeInTheDocument();
+});
+
+test('Verificação do elemento 2', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero2 = screen.getByText(/2/i);
+  expect(elementoNumero2).toBeInTheDocument();
+});
+
+test('Verificação do elemento 1', () => {
+  const { getByText } = render(<App />);
+  const elementoNumero1 = screen.getByText(/1/i);
+  expect(elementoNumero1).toBeInTheDocument();
+});
+
+
